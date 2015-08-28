45b61b3b7fc287c71424eddc4220d1c202d6fcb4
Christopher Welker <cwelker@cmc.edu>
2015-08-28 14:37:08 -0700

    added base template

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    title = "Hello World!"
+    return render_template("base.html",title=title)
 
 if __name__ == '__main__':
     app.run()
