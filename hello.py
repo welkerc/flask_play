929825bd81095270995bebe8d2d2a829132567c6
Christopher Welker <cwelker@cmc.edu>
2015-08-28 14:40:07 -0700

    Added greet route and template

--- hello.py
+++ hello.py
@@ -6,5 +6,10 @@ def hello_world():
     title = "Hello World!"
     return render_template("base.html",title=title)
 
+@app.route('/greet/<person>')
+def greet_person(person):
+    title = "Greetings!"
+    return render_template("greet.html",title=title,person=person)
+
 if __name__ == '__main__':
     app.run()
